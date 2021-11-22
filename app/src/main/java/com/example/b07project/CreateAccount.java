025bce629486363e7f0111906b676e1a3691300f
wilso929 <wilsonben@rogers.com>
2021-11-21 19:34:24 -0500

    Connected the CreateAccount activity with a firebase database

--- app/src/main/java/com/example/b07project/CreateAccount.java
+++ app/src/main/java/com/example/b07project/CreateAccount.java
@@ -1,8 +1,13 @@
 package com.example.b07project;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -12,4 +17,30 @@ public class CreateAccount extends AppCompatActivity {
         setContentView(R.layout.activity_create_account);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public void Create_Owner(View view){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        EditText text = findViewById(R.id.Username);
+        String username = text.getText().toString();
+        text = findViewById(R.id.Password);
+        String password = text.getText().toString();
+
+        DatabaseReference myRef = database.getReference("Owners/"+username+"/Username");
+        myRef.setValue(username);
+        myRef = database.getReference("Owners/"+username+"/Password");
+        myRef.setValue(password);
+    }
+
+    public void Create_Customer(View view){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        EditText text = findViewById(R.id.Username);
+        String username = text.getText().toString();
+        text = findViewById(R.id.Password);
+        String password = text.getText().toString();
+        DatabaseReference myRef = database.getReference("Customers/"+username+"/Username");
+        myRef.setValue(username);
+        myRef = database.getReference("Customers/"+username+"/Password");
+        myRef.setValue(password);
+    }
+
 }
