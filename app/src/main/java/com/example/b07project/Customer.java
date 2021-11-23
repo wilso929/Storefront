24acbfdff2e0780b9ca3c7f5c095cda9816cb8e5
aydinparekh <aydinparekh@gmail.com>
2021-11-23 13:25:14 -0500

    Added orders HashSet to customer and add_order method.

--- app/src/main/java/com/example/b07project/Customer.java
+++ app/src/main/java/com/example/b07project/Customer.java
@@ -1,6 +1,10 @@
 package com.example.b07project;
 
+import java.util.HashSet;
+
 public class Customer extends User{
+
+    HashSet<Order> orders;
 
     public Customer() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
@@ -9,5 +13,9 @@ public class Customer extends User{
     public Customer(String username, String password) {
 
         super(username, password);
+    }
+
+    public void add_order(Order o){
+        orders.add(o);
     }
 }
