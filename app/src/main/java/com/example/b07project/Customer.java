4a08f04bf8bb67eff962cfdf81091cbf72e51ac4
theresayuy <theresay.yu.2001@gmail.com>
2021-11-23 19:59:17 -0500

    Updated Customer.java

--- app/src/main/java/com/example/b07project/Customer.java
+++ app/src/main/java/com/example/b07project/Customer.java
@@ -1,13 +1,20 @@
 package com.example.b07project;
 
+import java.util.HashSet;
+
 public class Customer extends User{
 
+    // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    HashSet<Order> orders = new HashSet<>();
+
     public Customer() {
-        // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
     public Customer(String username, String password) {
-
         super(username, password);
+    }
+
+    public void add_order(Order o){
+        orders.add(o);
     }
 }
