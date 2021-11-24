acc210ceb0a1720521f68f548cce1e8c6e53ded0
Theresa Yu <69079112+theresayuy@users.noreply.github.com>
2021-11-23 19:58:23 -0500

    Update Customer.java
    Removed the line that initialized the HashSet orders from the constructor.

--- app/src/main/java/com/example/b07project/Customer.java
+++ app/src/main/java/com/example/b07project/Customer.java
@@ -4,17 +4,15 @@ import java.util.HashSet;
 
 public class Customer extends User{
 
-    HashSet<Order> orders;
+    HashSet<Order> orders = new HashSet<Order>();
 
     public Customer() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
-        this.orders = new HashSet<Order>();
     }
 
     public Customer(String username, String password) {
 
         super(username, password);
-        this.orders = new HashSet<Order>();
     }
 
     public void add_order(Order o){
