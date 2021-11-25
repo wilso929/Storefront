c7b54379a321c02e475628cb13346ef40290614f
wilso929 <wilsonben@rogers.com>
2021-11-24 23:37:09 -0500

    Fix issue between Login and Owner landing page

--- app/src/main/java/com/example/b07project/DisplayOwnerActivity.java
+++ app/src/main/java/com/example/b07project/DisplayOwnerActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayOwnerActivity extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class DisplayOwnerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_owner);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         if(intent.getExtras() !=null){
@@ -25,6 +27,8 @@ public class DisplayOwnerActivity extends AppCompatActivity {
         for(Product p : owner.getProduct_list()){
             Log.i("pro", p.getName());
         }
+        Alert("Pass", owner.getPassword());
+
 
     }
 
@@ -35,5 +39,16 @@ public class DisplayOwnerActivity extends AppCompatActivity {
     }
 
     public void view_orders(View view){}
+
+    public void Alert(String title, String msg){
+        AlertDialog.Builder builder = new AlertDialog.Builder(DisplayOwnerActivity.this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(msg);
+        builder.setNegativeButton("Close", (dialog, which) -> dialog.cancel());
+        builder.show();
+    }
+
+
 
 }
--- app/src/main/java/com/example/b07project/DisplayOwnerProductsActivity.java
+++ app/src/main/java/com/example/b07project/DisplayOwnerProductsActivity.java
@@ -1,5 +1,6 @@
 package com.example.b07project;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -16,15 +17,26 @@ public class DisplayOwnerProductsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_owner_products);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         if(intent.getExtras() !=null){
             owner = (Owner) intent.getSerializableExtra("Owner");
         }
+        Alert("Pass", owner.getPassword());
 
         listview = findViewById(R.id.listview_products);
 
         ArrayAdapter<Product> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, owner.product_list);
         listview.setAdapter(adapter);
+    }
+
+    public void Alert(String title, String msg){
+        AlertDialog.Builder builder = new AlertDialog.Builder(DisplayOwnerProductsActivity.this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(msg);
+        builder.setNegativeButton("Close", (dialog, which) -> dialog.cancel());
+        builder.show();
     }
 }
