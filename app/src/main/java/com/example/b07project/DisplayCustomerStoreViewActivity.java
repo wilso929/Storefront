f85642fe370203c2846a4b7c633173238127cb2e
Cindy-Ji <cindy_ji02@outlook.com>
2021-11-26 23:27:24 +0800

    set up an empty page for customers to view stores

---
+++ app/src/main/java/com/example/b07project/DisplayCustomerStoreViewActivity.java
@@ -0,0 +1,19 @@
+package com.example.b07project;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisplayCustomerStoreViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayStore(View view){
+
+    }
+}
